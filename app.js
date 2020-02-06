44bb24b96769f7a2052b4634b9b9962d85d69551
Druhin Tarafder <druhinpro@gmail.com>
2020-02-06 20:48:45 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -12,12 +12,14 @@ var h5 = ['a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', 
 var h6 = ['a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 
-var color1 = 'background-color: #' + random_color(h1) + random_color(h2) + random_color(h3) + random_color(h4) + random_color(h5) + random_color(h6);
+var color1 = '#' + random_color(h1) + random_color(h2) + random_color(h3) + random_color(h4) + random_color(h5) + random_color(h6);
 
-var color2 = 'background-color: #' + random_color(h1) + random_color(h2) + random_color(h3) + random_color(h4) + random_color(h5) + random_color(h6);
+var color2 = '#' + random_color(h1) + random_color(h2) + random_color(h3) + random_color(h4) + random_color(h5) + random_color(h6);
+
+var gradient = 'background-image: linear-gradient(to right, ' + color1 + ' , ' + color2 + ')';
 
 var element = document.getElementById("color");
-element.style = color1;
+element.style = gradient;
 
 console.log(color1);
 console.log(color2);
