f56471e07a6b7a5af29101a4257cfdee487eb6b7
Druhin Tarafder <druhinpro@gmail.com>
2020-02-06 20:05:52 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+function random_color(items) {
 
+	return items[Math.floor(Math.random() * items.length)];
+
+}
+
+var h1 = ['a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+
+var color = random_color(h1) + 'b';
+console.log(color);
